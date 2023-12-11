de79dd8539ad73952b5fe43e1a7c3f6646673274
Stefan Gränitz <stefan.graenitz@gmail.com>
2023-12-11 22:36:51 +0100

    Add prime_factors example with complex return type Vec<u64>

---
+++ prime_factors.rs
@@ -0,0 +1,14 @@
+pub fn prime_factors(mut n: u64) -> Vec<u64> {
+    let mut factors = Vec::new();
+    let mut candidate = 2;
+
+    while n > 1 {
+        while n % candidate == 0 {
+            factors.push(candidate);
+            n /= candidate;
+        }
+        candidate += 1;
+    }
+
+    factors
+}
